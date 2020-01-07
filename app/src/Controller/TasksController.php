0eb6d1d707d8917c2d4ecc4243ea7515df38c5fb
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-07 12:14:14 -0500

    Created tasks model, controller, and template

---
+++ app/src/Controller/TasksController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class TasksController extends AppController
+{
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $tasks = $this->Paginator->paginate($this->Tasks->find());
+        $this->set(compact('tasks'));
+    }
+}
---
+++ app/templates/Tasks/index.php
@@ -0,0 +1,26 @@
+<h1>Tasks</h1>
+<table>
+    <tr>
+        <th>Task Name</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Assigned User</th>
+    </tr>
+
+    <?php foreach ($tasks as $task): ?>
+    <tr>
+        <td>
+            <?= $task->task_name ?>
+        </td>
+        <td>
+            <?= $task->description ?>
+        </td>
+        <td>
+            <?= $task->status ?>
+        </td>
+        <td>
+            <?= $task->user_id ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
