eafddc57ba1f26bc3915e9059358b82a981fea0a
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-08 01:30:30 -0500

    troubleshooting

--- app/templates/Tasks/index.php
+++ app/templates/Tasks/index.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html>
-    <head></head>
-    <body>
-    <h1>Tasks</h1>
-        <table>
-            <tr>
-                <th>Task Name</th>
-                <th>Description</th>
-                <th>Status</th>
-                <th>Assigned User</th>
-            </tr>
+<h1>Tasks</h1>
+    <table>
+        <tr>
+            <th>Task Name</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Assigned User</th>
+        </tr>
 
-            <?php foreach ($tasks as $task): ?>
-            <tr>
-                <td>
-                    <?= $task->task_name ?>
-                </td>
-                <td>
-                    <?= $task->description ?>
-                </td>
-                <td>
-                    <?= $task->status ?>
-                </td>
-                <td>
-                    <?= $task->user_id ?>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </table>    
-    </body>
-</html>
-
+        <?php foreach ($tasks as $task): ?>
+        <tr>
+            <td>
+                <?= $task->task_name ?>
+            </td>
+            <td>
+                <?= $task->description ?>
+            </td>
+            <td>
+                <?= $task->status ?>
+            </td>
+            <td>
+                <?= $task->user_id ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>    
