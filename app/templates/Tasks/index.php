cb5b9639323d72ebc9dc7392a894036c5f731b1a
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-07 23:46:09 -0500

    structured html of index.php

--- app/templates/Tasks/index.php
+++ app/templates/Tasks/index.php
@@ -1,26 +1,33 @@
-<h1>Tasks</h1>
-<table>
-    <tr>
-        <th>Task Name</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th>Assigned User</th>
-    </tr>
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+    <h1>Tasks</h1>
+        <table>
+            <tr>
+                <th>Task Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Assigned User</th>
+            </tr>
 
-    <?php foreach ($tasks as $task): ?>
-    <tr>
-        <td>
-            <?= $task->task_name ?>
-        </td>
-        <td>
-            <?= $task->description ?>
-        </td>
-        <td>
-            <?= $task->status ?>
-        </td>
-        <td>
-            <?= $task->user_id ?>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+            <?php foreach ($tasks as $task): ?>
+            <tr>
+                <td>
+                    <?= $task->task_name ?>
+                </td>
+                <td>
+                    <?= $task->description ?>
+                </td>
+                <td>
+                    <?= $task->status ?>
+                </td>
+                <td>
+                    <?= $task->user_id ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>    
+    </body>
+</html>
+
