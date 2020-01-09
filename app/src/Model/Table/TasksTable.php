94378102b0f28519ff4a5636ecbf6367dc722b61
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-09 06:32:30 -0500

    testing validation

--- app/src/Model/Table/TasksTable.php
+++ app/src/Model/Table/TasksTable.php
@@ -3,11 +3,27 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class TasksTable extends Table
 {
     public function initialize(array $config): void
     {
         
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->allowEmptyString('task_name', false)
+            ->minLength('task_name', 5)
+            ->maxLength('task_name', 25)
+
+            ->allowEmptyString('description', false)
+            ->minLength('description', 5)
+            ->maxLength('description', 1000)
+
+            ->allowEmptyString('status', false)
+            ->inList('status', ['Not Started', 'In Progress', 'Completed']); 
     }
 }
