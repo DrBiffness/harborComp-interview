b464974ed59a8a76b409e05fceb4ea8ecef3884c
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-09 15:31:16 -0500

    made changes to validation for tasks

--- app/src/Model/Table/TasksTable.php
+++ app/src/Model/Table/TasksTable.php
@@ -15,15 +15,24 @@ class TasksTable extends Table
     public function validationDefault(Validator $validator): Validator
     {
         $validator
-            ->allowEmptyString('task_name', false)
+            ->allowEmptyString('id', false);
+
+        $validator
+            ->scalar('task_name')
             ->minLength('task_name', 5)
             ->maxLength('task_name', 25)
-
-            ->allowEmptyString('description', false)
-            ->minLength('description', 5)
+            ->requirePresence('task_name', 'create')
+            ->notEmptyString('task_name');
+        
+        $validator
+            ->scalar('description')
             ->maxLength('description', 1000)
+            ->requirePresence('description', 'create')
+            ->notEmptyString('description');
 
-            ->allowEmptyString('status', false)
+        $validator
+            ->scalar('status')
+            ->notEmptyString('status', false)
             ->inList('status', ['Not Started', 'In Progress', 'Completed']);
             
         return $validator;
