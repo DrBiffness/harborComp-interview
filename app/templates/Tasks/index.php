15274641a0ddbe59bd122c4dd494660e820a06c8
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-10 04:39:52 -0500

    troubleshooting joins on home page

--- app/templates/Tasks/index.php
+++ app/templates/Tasks/index.php
@@ -9,6 +9,7 @@
                     <th><?= $this->Paginator->sort('description') ?></th>
                     <th><?= $this->Paginator->sort('status') ?></th>
                     <th><?= $this->Paginator->sort('username') ?></th>
+                    <th><?= $this->Paginator->sort('username') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
@@ -19,6 +20,7 @@
                     <td><?= h($task->description) ?></td>
                     <td><?= h($task->status) ?></td>
                     <td><?= h($task->username) ?></td>
+                    <td><?= h($task->user_id->username) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $task->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $task->id], ['confirm' => __('Are you sure you want to delete task: "{0}"?', $task->task_name)]) ?>
