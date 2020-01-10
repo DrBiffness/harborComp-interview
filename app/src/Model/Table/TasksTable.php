14b762760817b35de88f1287ec69ecc343a3846c
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-09 22:38:52 -0500

    troubleshooting join on home page

--- app/src/Model/Table/TasksTable.php
+++ app/src/Model/Table/TasksTable.php
@@ -10,7 +10,9 @@ class TasksTable extends Table
     public function initialize(array $config): void
     {
         parent::initialize($config);
-        $this->hasOne('Users');
+        $this->hasOne('Users', [
+            'className' => 'Users'
+        ]);
     }
 
     public function validationDefault(Validator $validator): Validator
