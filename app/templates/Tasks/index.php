d15ca862ff4d95637be43620203d2a44ff672af8
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2020-01-10 04:54:10 -0500

    troubleshooting home page

--- app/templates/Tasks/index.php
+++ app/templates/Tasks/index.php
@@ -9,20 +9,16 @@
                     <th><?= $this->Paginator->sort('description') ?></th>
                     <th><?= $this->Paginator->sort('status') ?></th>
                     <th><?= $this->Paginator->sort('username') ?></th>
-                    <th><?= $this->Paginator->sort('username') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($tasks as $task): ?>
-                    
-                <?= echo $task ?>
                 <tr>
                     <td><?= h($task->task_name) ?></td>
                     <td><?= h($task->description) ?></td>
                     <td><?= h($task->status) ?></td>
-                    <td><?= h($task->username) ?></td>
-                    <td><?= h($task->user_id->username) ?></td>
+                    <td><?= h($task->User) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $task->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $task->id], ['confirm' => __('Are you sure you want to delete task: "{0}"?', $task->task_name)]) ?>
